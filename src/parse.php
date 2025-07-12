f2d3ca18518f818b0c34143997653746fba637f1
keep silent <learn.fllutter@gmail.com>
2025-07-12 22:02:08 +0500

    added parse and print_r to screen files

---
+++ src/parse.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Hexlet\Code;
+
+function parse($path)
+{
+    //if (!file_exists($path)) {
+    //    throw new \Exception ("File not found!: {$path}");
+    //}
+    $contents = file_get_contents($path);
+    $data = json_decode($contents, true);
+    return $data;
+}
